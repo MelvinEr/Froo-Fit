46ae0a7570b32fc68bb56e322d96c36894fe5cac
Melvin <melvinerikssonjobb@gmail.com>
2022-03-14 22:29:58 +0100

    axel you whore, host the db already

--- src/preload.js
+++ src/preload.js
@@ -1,18 +1,1 @@
 const { contextBridge } = require("electron");
-const mysql = require("mysql");
-
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: null,
-  database: "froofit_db",
-});
-
-connection.connect();
-let sql = 'SELECT * FROM `user`';
-connection.query(sql, function(error, results, fields) {
-    if(error) console.log(error.code);
-    else {
-        console.log(results);
-    }
-})
--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("dom content loaded");
+  //getUsers();
+});
+
 document.getElementById("muscle-groups").addEventListener("click", async () => {
-  // document.getElementById("content").innerHTML = "Muscle group ui goes here";
-  document.getElementById("content").innerHTML = "Muscle groups ui goes here";
+  // ok so you can inject html directly like on line 5, but this is really autistic and surely there's a better way.
+  // ideally what i'd like to do is something like this V
+  // document.getElementById("content").innerHTML = "<muscle-groups>";
+  document.getElementById("content").innerHTML = "<h1> Muscle groups ui goes here </h1>";
 });
 
 document.getElementById("register").addEventListener("click", async () => {
-  document.getElementById("content").innerHTML = "Register ui goes here";
+  document.getElementById("content").innerHTML = "<h1> Register ui goes here </h1>";
 });
 
 document.getElementById("store").addEventListener("click", async () => {
-  document.getElementById("content").innerHTML = "Froocoin store goes here?";
+  document.getElementById("content").innerHTML = "<h1> Froocoin store goes here? </h1>";
 });
